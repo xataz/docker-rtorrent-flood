02ffc7f701b6eabe0de1bdbc241b4e56ec818408
xataz <xataz@mondedie.fr>
2018-05-02 19:09:42 +0200

    Edit default config

--- rootfs/app/flood/config.js
+++ rootfs/app/flood/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  baseURI: process.env.WEBROOT || '/',
+  baseURI: process.env.FLOOD_BASE_URI || '/',
   dbCleanInterval: 1000 * 60 * 60,
   dbPath: '/flood-db/',
   floodServerHost: '0.0.0.0',
@@ -9,9 +9,9 @@ const CONFIG = {
   torrentClientPollInterval: 1000 * 2,
   secret: process.env.FLOOD_SECRET || 'secret',
   scgi: {
-    host: 'localhost',
-    port: 5000,
-    socket: true,
+    host: process.env.RTORRENT_SCGI_HOST || 'localhost',
+    port: process.env.RTORRENT_SCGI_PORT || 5000,
+    socket: process.env.RTORRENT_SOCK == 'true',
     socketPath: '/tmp/rtorrent.sock'
   },
   ssl: false,
