4a3d7871999a31d73131bd7202dbca5dc18886a1
xataz <xataz@users.noreply.github.com>
2018-05-02 16:04:28 +0200

    Update config.js

--- rootfs/app/flood/config.js
+++ rootfs/app/flood/config.js
@@ -2,16 +2,22 @@ const CONFIG = {
   baseURI: '<WEBROOT>',
   dbCleanInterval: 1000 * 60 * 60,
   dbPath: '/flood-db/',
+  floodServerHost: '0.0.0.0',
   floodServerPort: 3000,
+  floodServerProxy: '',
   maxHistoryStates: 30,
   pollInterval: 1000 * 5,
+  torrentClientPollInterval: 1000 * 2,
   secret: '<FLOOD_SECRET>',
   scgi: {
     host: 'localhost',
     port: 5000,
     socket: true,
     socketPath: '/tmp/rtorrent.sock'
-  }
+  },
+  ssl: false,
+  sslKey: '',
+  sslCert: ''
 };
 
 module.exports = CONFIG;
